ba3e758ed0957c4294bfaf84855ad4b5637d3614
Thiago Pereira <titiura@gmail.com>
2024-11-17 13:33:28 -0300

    Chore: install @nestjs/config and update main.ts for dotenv integration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+
+  const port = process.env.PORT;
+  await app.listen(port);
+  console.log('Server running on port ${port}');
 }
 bootstrap();
