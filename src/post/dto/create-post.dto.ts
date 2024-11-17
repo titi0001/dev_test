96aa1f2c9472ad0a2040f086b72ba63e4e5a41bb
Thiago Pereira <titiura@gmail.com>
2024-11-17 19:27:20 -0300

    feat: add DTO´s for user and post validation

---
+++ src/post/dto/create-post.dto.ts
@@ -0,0 +1,15 @@
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  userId: number;
+}
