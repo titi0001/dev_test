021c06390cccbbe5848da2505ab4eabf05f3468c
Thiago Pereira <titiura@gmail.com>
2024-11-17 14:04:58 -0300

    refactor: Remove unused AppController/AppService, update AppModule-PostModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, PostModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+    PostModule,
+  ],
 })
 export class AppModule {}
--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PostController } from './post.controller';
-import { ServiceModule } from './service/service.module';
 import { PostService } from './post.service';
 
 @Module({
   controllers: [PostController],
-  imports: [ServiceModule],
   providers: [PostService]
 })
 export class PostModule {}
