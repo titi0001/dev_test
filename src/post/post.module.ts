458fe6f0152651761945c326450d8d91b00dc26b
Thiago Pereira <titiura@gmail.com>
2024-11-17 13:40:51 -0300

    refactor: Remove incorrectly created service.module.ts and update post service module

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PostController } from './post.controller';
 import { ServiceModule } from './service/service.module';
+import { PostService } from './post.service';
 
 @Module({
   controllers: [PostController],
-  imports: [ServiceModule]
+  imports: [ServiceModule],
+  providers: [PostService]
 })
 export class PostModule {}
