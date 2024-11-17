6a5d420ff993f8fe719fbdd81c225c6f45124ebf
Thiago Pereira <titiura@gmail.com>
2024-11-17 18:26:39 -0300

    feat: add db creation and table definitions in init.sql script

--- init.sql
+++ init.sql
@@ -1,5 +1,17 @@
+CREATE DATABASE IF NOT EXISTS test_db;
 USE test_db;
 
---TODO Crie a tabela de user;
+CREATE TABLE IF NOT EXISTS user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(100) NOT NULL,
+    lastName VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
 
---TODO Crie a tabela de posts;
+CREATE TABLE IF NOT EXISTS post (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    userId INT NOT NULL,
+    FOREIGN KEY (userId) REFERENCES user(id) ON DELETE CASCADE
+);
