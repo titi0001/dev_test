ba40cc4277b62aaf45a5f4527c7f51b283f16f94
Thiago Pereira <titiura@gmail.com>
2024-11-17 19:30:12 -0300

    refactor: update main.ts to align to validate

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   dotenv.config();
 
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
 
   const port = process.env.PORT;
