51a716493f3964c3acb8bc214d5548d85e48e553
Thiago Pereira <titiura@gmail.com>
2024-11-17 22:34:35 -0300

    refactor: improve database connection management

--- src/db/appDataSource.ts
+++ src/db/appDataSource.ts
@@ -1,17 +1,22 @@
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { DataSource } from 'typeorm';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 
-export const createAppDataSource = (
-  configService: ConfigService,
-): DataSource => {
-  return new DataSource({
-    type: 'mysql',
-    host: configService.get<string>('DB_HOST_APP'),
-    port: configService.get<number>('DB_PORT') || 3306,
-    username: configService.get<string>('DB_USER'),
-    password: configService.get<string>('DB_PASSWORD'),
-    database: configService.get<string>('DB_NAME'),
-    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-    synchronize: configService.get<boolean>('TYPEORM_SYNC') || false,
-  });
-};
+@Injectable()
+export class AppDataSource implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: this.configService.get<string>('DB_HOST', 'localhost'),
+      port: this.configService.get<number>('DB_PORT', 3306),
+      username: this.configService.get<string>('DB_USER'),
+      password: this.configService.get<string>('DB_PASSWORD'),
+      database: this.configService.get<string>('DB_NAME'),
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: true,
+      autoLoadEntities: true,
+    };
+  }
+}
