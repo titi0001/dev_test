765078adedc61be6eef4131f8a20cdc6d462f7f7
Thiago Pereira <titiura@gmail.com>
2024-11-17 22:34:54 -0300

    refactor: adjust CLI for database initialization

--- src/db/data-source.cli.ts
+++ src/db/data-source.cli.ts
@@ -1,7 +1,17 @@
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { createAppDataSource } from './appDataSource';
+import 'dotenv/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
 
-ConfigModule.forRoot();
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST_APP,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/*{.ts,.js}'],
+};
 
-const configService = new ConfigService();
-export const AppDataSource = createAppDataSource(configService);
+const AppDataSource = new DataSource(dataSourceOptions);
+
+export default AppDataSource;
