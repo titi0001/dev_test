df8132837a9ede6652e3625c7653393dfa106657
Thiago Pereira <titiura@gmail.com>
2024-11-17 22:38:23 -0300

    refactor: remove dotenv to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  dotenv.config();
-
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
 
-  const port = process.env.PORT;
+  const port = 3000;
   await app.listen(port);
-  console.log('Server running on port ${port}');
+  console.log(`Server running on port ${port}`);
 }
 bootstrap();
